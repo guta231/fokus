df14363392c69a214305992bb3d65fe3914db46a
guta231 <gustavoho2005@gmail.com>
2025-04-07 23:02:43 -0300

    conclusão módulo 1 e módulo 2

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,82 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.jsx to edit this screen.</Text>
+      <Image source={require("./pomodoro.png")} style={styles.image}/>
+      <View style={styles.action}>
+
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins lucrativos. Desenvolvido por Alura.
+        </Text>
+      </View>
     </View>
   );
 }
+
+
+
+
+
+const styles = StyleSheet.create({
+
+  container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#021123",
+      gap: 40,
+  },
+  image: {
+    width: "90%",
+    height: "45%",
+  
+  },
+  action: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    color: "#fff",
+    textAlign: "center",
+
+  },
+  button: {
+    backgroundColor: "#8872ff",
+    padding: 8,
+    borderRadius: 32,
+
+
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+
+  },
+  footer: {
+    width: "80%",
+  },
+  footerText: {
+    textAlign: "center",
+    color: "#98A0A8",
+    fontSize: 12.5,
+  }
+
+})
